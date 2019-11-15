471d47e4c5862fb1f0f60975a221ff4d5a36de3a
Github-zs <38308786+github-zs@users.noreply.github.com>
2019-11-15 16:25:47 +0800

    refactoring multiple module project

---
+++ mq-consumer/src/main/java/com/zs/consumer/MessageConsumer.java
@@ -0,0 +1,16 @@
+
+package com.zs.consumer;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <pre>
+ *    描述信息
+ * </pre>
+ *
+ * @author zhaishuo
+ * @version $Id: MessageConsumer.java, v1.0 2019/11/15 16:19 zhaishuo Exp $
+ */
+@Component
+public class MessageConsumer {
+}
