de5112a89a0dd2c013b3f745929e454c1f439d64
Zhai Shuo <38308786+github-zs@users.noreply.github.com>
2020-04-06 20:03:30 +0800

    user service integration security

--- spring-cloud-user/src/main/java/com/zs/springclouduser/controller/UserController.java
+++ spring-cloud-user/src/main/java/com/zs/springclouduser/controller/UserController.java
@@ -2,19 +2,40 @@ package com.zs.springclouduser.controller;
 
 import com.zs.springclouduser.bo.User;
 import com.zs.springclouduser.mapper.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserMapper mapper;
 
     @GetMapping("/{id}")
     public User fingById(@PathVariable int id) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            UserDetails  user = (UserDetails)principal;
+            Collection<? extends GrantedAuthority> collection = user.getAuthorities();
+            for (GrantedAuthority auth: collection) {
+                logger.info("当前用户是: {}, 角色是: {}", user.getUsername(), auth.getAuthority());
+            }
+        } else {
+            logger.info("用户验证未通过");
+        }
+
         return mapper.selectByPrimaryKey(id);
     }
 
---
+++ spring-cloud-user/src/main/java/com/zs/springclouduser/security/WebSecurityConfig.java
@@ -0,0 +1,136 @@
+package com.zs.springclouduser.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailService userDetailService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        /*
+        明文编码器，对密码不做任何操作，对应有md5等密码加密编码器，spring5以后以及不建议使用。
+         */
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //设置所有请求都要通过http basic认证
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailService).passwordEncoder(this.passwordEncoder());
+    }
+
+    /*
+    用户验证service，正常项目中使用此处需要查询数据库。
+     */
+    @Component
+    class CustomUserDetailService implements UserDetailsService {
+
+        /*
+        模拟两个用户。
+         */
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            if ("user".equals(username)) {
+                return new SecurityUser("user", "password1", "user-role");
+            } else if ("admin".equals(username)) {
+                return new SecurityUser("admin", "password2", "admin-role");
+            } else {
+                return null;
+            }
+        }
+    }
+
+    /*
+    模拟用户
+     */
+    class SecurityUser implements UserDetails {
+
+        private long id;
+        private String username;
+        private String password;
+        private String role;
+
+        public SecurityUser(String username, String password, String role) {
+            super();
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
+
+        public SecurityUser() {
+            super();
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(this.role);
+            authorities.add(authority);
+            return authorities;
+        }
+
+        @Override
+        public String getPassword() {
+            return this.password;
+        }
+
+        @Override
+        public String getUsername() {
+            return this.username;
+        }
+
+        /*
+        测试过程中以下值都需要设置为true  不然用户不可用登陆不成功。
+         */
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+
+}
